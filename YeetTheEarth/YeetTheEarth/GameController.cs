c34ec75012cf287cf847cec78072d461f2a75ab5
Caleb Logan <catiel@ksu.edu>
2018-10-20 00:37:50 -0500

    zIM so CONFUSED

--- YeetTheEarth/YeetTheEarth/GameController.cs
+++ YeetTheEarth/YeetTheEarth/GameController.cs
@@ -8,6 +8,39 @@ namespace YeetTheEarth
 {
     public class GameController
     {
+        private Player _player;
+        private Earth _earth;
 
+        public GameController()
+        {
+            _player = new Player();
+            _earth = new Earth();
+
+            _player.ShowGameIntroMessage();
+
+            while(_earth.Population > 0)
+            {
+                //Show stuff about Earth's current state
+                _player.Show(String.Format("Year: {}", _earth.Year));
+                _player.Show(String.Format("Month: {}", _earth.Month));
+                _player.Show(String.Format("Population: {} people", _earth.Population));
+                _player.Show(String.Format("Political Capital: {} points", _earth.PoliticalCapital));
+                _player.Show(String.Format("Average Temperature: {} °C", _earth.Temperature));
+                _player.Show(String.Format("Sea Level: {} m", _earth.SeaLevel));
+                _player.Show(String.Format("Global GDP: ${}", _earth.GDP));
+                
+                //Generate random events sometimes
+                //If there is an event in effect:
+                    //For each event:
+                        //Show player event message for this month
+                        //Get choice from player
+                        //Advance event one month
+
+                //Show player normal monthly options
+                //Get decisions from player
+
+                _earth.NextMonth();
+            }
+        }
     }
 }
