e96e903a25af585cbdf3eb0c2b9fba9494068bab
Caleb Logan <catiel@ksu.edu>
2018-10-19 22:17:39 -0500

    klas

---
+++ YeetTheEarth/YeetTheEarth/Earth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.
+
+namespace YeetTheEarth
+{
+    class Earth
+    {
+        private double _temp;
+
+        public double Temp
+        {
+            get
+            {
+                return _temp;
+            }
+            set
+            {
+                _temp = GetRandomNumber(0, 160);
+            }
+        }
+
+
+
+
+        static Random random = new Random();
+        public double GetRandomNumber(double minimum, double maximum)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
