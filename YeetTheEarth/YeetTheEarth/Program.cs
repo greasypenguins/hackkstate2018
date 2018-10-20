fe046cf2826d3703433a8b556b2217062e183a0b
Stephanie Milberger <stephanie.milberger@gmail.com>
2018-10-20 00:57:16 -0500

    :))))

--- YeetTheEarth/YeetTheEarth/Program.cs
+++ YeetTheEarth/YeetTheEarth/Program.cs
@@ -11,7 +11,21 @@ namespace YeetTheEarth
         static void Main(string[] args)
         {
 
-            
+            //Print what month it is/how many months have passed
+
+            //Print a random event and fact for them to understand
+
+            //Print all of the options for the events
+
+            //Read in the the user input
+
+            //Calculate the condition and all of the variables
+
+            //Calculate difficulty level with AI
+
+            //Print the condition and variables as well as facts and tips/warnings
+
+            //Print next month
         }
     }
 }
