9b5e3509e272e60ae9036f606968caad53f4ede4
Caleb Logan <catiel@ksu.edu>
2018-10-20 00:54:07 -0500

    i figured it out hahah fuck you.

--- YeetTheEarth/YeetTheEarth/Earth.cs
+++ YeetTheEarth/YeetTheEarth/Earth.cs
@@ -45,18 +45,101 @@ namespace YeetTheEarth
             }
             set
             {
-                _month = new string[12] {"January", "February", "March", "April", "May","June", "July", "August", "September","October" , "November", "December"};
-                
+                _month = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
 
             }
         }
-        
+        private double _tempRate;
+        public double TempRate
+        {
+            get
+            {
+                return _tempRate;
+            }
+        }
+
+        private double _co2Con;
+        public double Co2Con
+        {
+            get
+            {
+                return _co2Con;
+            }
+        }
+
+        private double _co2Rate;
+        public double Co2Rate
+        {
+            get
+            {
+                return _co2Rate;
+            }
+        }
+
+        private double _gdp;
+        public double GDP
+        {
+            get
+            {
+                return _gdp;
+            }
+        }
+
+        private int _population;
+        public int Population
+        {
+            get
+            {
+                return _population;
+            }
+        }
+
+        private int _seaLevel;
+        public int SeaLevel
+        {
+            get
+            {
+                return _seaLevel;
+            }
+        }
+
+        private double _seaRate;
+        public double SeaRate
+        {
+            get
+            {
+                return _seaLevel;
+            }
+        }
+
+        private int _politicPoints = 3;
+        public int PoliticPoints
+        {
+            get
+            {
+                return _politicPoints;
+            }
+            set
+            {
+                _politicPoints++;
+            }
+        }
+
+        private double _polarCaps;
+        public double PolarCap
+        {
+            get
+            {
+                return _polarCaps;
+            }
+        }
 
 
         public void AdvanceMonth(string month)
         {
             _indexMonth++;
-            CurrentMonth = Month[_indexMonth]; 
+            CurrentMonth = Month[_indexMonth];
         }
     }
 }
---
+++ YeetTheEarth/YeetTheEarth/Energy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeetTheEarth
+{
+    class Energy
+    {
+        private Energy _solar;
+        public Energy Solar
+        {
+            get;
+            set;
+        }
+        private Energy _hydro;
+        public Energy Hydro
+        {
+            get;
+            set;
+        }
+        private Energy _geothermic;
+        public Energy Geothermic
+        {
+            get;
+            set;
+        }
+        private Energy _wind;
+        public Energy Wind
+        {
+            get;
+            set;
+        }
+        private Energy _nuclear;
+        public Energy Nuclear
+        {
+            get;
+            set;
+        }
+        private Energy _coal;
+        public Energy Coal
+        {
+            get;
+            set;
+        }
+        private Energy _naturalGas;
+        public Energy NaturalGas
+        {
+            get;
+            set;
+        }
+
+
+        public Energy(Energy e)
+        {
+
+        }
+
+    }
+}
