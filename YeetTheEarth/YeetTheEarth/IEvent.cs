f3af97c0cd721c98fbf81dd885b5b3583476aa6c
greasypenguins <mrwestonharder@gmail.com>
2018-10-20 05:17:07 -0500

    Add IEvent interface

---
+++ YeetTheEarth/YeetTheEarth/IEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeetTheEarth
+{
+    interface IEvent
+    {
+        string StartMessage { get; } //Message displayed when event is triggered
+        
+        int MonthsLeft { get; } //Remaining duration of event in months
+
+        string NextMonth(); //Advance the event one month and get a message
+
+        string ChooseOption(int option); //Choose one of the options and get the result
+        
+    }
+}
